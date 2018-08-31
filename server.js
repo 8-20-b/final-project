f5f1f33d853cbd545611c7939dac728627017e7d
Dioni Mercado <me@dionimercado.com>
2018-08-31 17:03:24 -0400

    Now the app dynamically recognize the node environment.

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 // module imports
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const app = express();
 
-// PRODUCTION ONLY
-// app.use(express.static(path.join(__dirname, 'client/build')));
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 
+//PRODUCTION ONLY
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
 // app middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // PRODUCTION ONLY
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/client/build/index.html'));
-// });
-
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
 // Development mode port
 const port = process.env.PORT || 5000;
-app.listen(port)
+app.listen(port);
 
 module.exports = app;
