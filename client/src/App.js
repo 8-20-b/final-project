47b4664154a4a9036977e3af9453b050120138c7
Dioni Mercado <me@dionimercado.com>
2018-09-04 23:09:06 -0400

    Set up frontend project structure and React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-//import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import { Home, Login, Register } from "./containers/";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> */}
-        <h1 className="App-intro">
-          Here is where the front-end of our app will live
-        </h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <React.Fragment>
+      <Header brand="Movies Reviews" />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </React.Fragment>
+  </Router>
+);
 
 export default App;
