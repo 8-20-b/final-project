eb58ccdc0371422a467a7c064cd42fdf52806c99
Charsta Denise Scott <charstascott@gmail.com>
2018-09-08 13:28:07 -0400

    Added user signup form in frotend

--- client/src/containers/Register.js
+++ client/src/containers/Register.js
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
+import SignUpForm from '../components/Forms/SignUp';
 
 export default class Register extends Component {
+  state = {
+    email: "",
+    password: "",
+    password2: "",
+    errors: {}
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Submitted...')
+  }
+
   render() {
+
     return (
-      <div>
-        <h1>Registration page</h1>
+      <div className="mt-5">
+        <h1 className="text-center">Registration page</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-7 m-auto">
+              <div className="bg-info p-5 text-white">
+              <SignUpForm onChange={this.handleChange} onSubmit={this.handleSubmit} email={this.state.email} password={this.state.password} password2={this.state.password2} />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
