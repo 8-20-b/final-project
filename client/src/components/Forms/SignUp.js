c302009b5a0b652eab933434ed482bf6424f2a50
Charsta Denise Scott <charstascott@gmail.com>
2018-09-08 14:10:47 -0400

    Added input validations in SignUpForm

--- client/src/components/Forms/SignUp.js
+++ client/src/components/Forms/SignUp.js
@@ -5,10 +5,12 @@ const SignUp = props => (
   <div className="form-group">
     <label htmlFor="email">Email address</label>
     <input type="email" className="form-control" name="email" id="email" onChange={props.onChange} value={props.email} />
+    {props.errors.email ? <small className="text-danger">{props.errors.email}</small> : ""}
   </div>
   <div className="form-group">
     <label htmlFor="password1">Password</label>
     <input type="password" className="form-control" name="password" id="password1"  onChange={props.onChange} value={props.password} />
+    {props.errors.password ? <small className="text-danger">{props.errors.password}</small> : ""}
   </div>
   <div className="form-group">
     <label htmlFor="password2">Confirm password</label>
--- client/src/containers/Register.js
+++ client/src/containers/Register.js
@@ -16,11 +16,27 @@ export default class Register extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
 
+    const {email, password, password2} = this.state
+    const errors = this.validate({email, password, password2});
+    this.setState({errors});
+
     console.log('Submitted...')
   }
 
-  render() {
+  validate = data => {
+    
+    const errors = {};
+    
+    !data.email ? errors.email = "Enter your email address." : "";
+    !data.password ? errors.password = "Enter your password." : "";
+    data.password !== data.password2 ? errors.password = "Your passwords doesn't match." : "";
+    
+    console.log('data', errors);
+    return errors;
+  }
 
+  render() {
+    console.log('errors', this.state.errors)
     return (
       <div className="mt-5">
         <h1 className="text-center">Registration page</h1>
@@ -28,7 +44,7 @@ export default class Register extends Component {
           <div className="row">
             <div className="col-md-7 m-auto">
               <div className="bg-info p-5 text-white">
-              <SignUpForm onChange={this.handleChange} onSubmit={this.handleSubmit} email={this.state.email} password={this.state.password} password2={this.state.password2} />
+              <SignUpForm onChange={this.handleChange} onSubmit={this.handleSubmit} errors={this.state.errors} email={this.state.email} password={this.state.password} password2={this.state.password2} />
               </div>
             </div>
           </div>
