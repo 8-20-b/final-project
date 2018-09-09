c2c88a76f7833fc86dff60aef59cc1223331e5f5
Dioni Mercado <me@dionimercado.com>
2018-09-09 12:34:48 -0400

    Now rendering backend validations in frontend and some other improvements

--- client/src/components/Forms/SignUp.js
+++ client/src/components/Forms/SignUp.js
@@ -1,23 +1,53 @@
-import React from 'react';
+import React from "react";
 
 const SignUp = props => (
-<form>
-  <div className="form-group">
-    <label htmlFor="email">Email address</label>
-    <input type="email" className="form-control" name="email" id="email" onChange={props.onChange} value={props.email} />
-    {props.errors.email ? <small className="text-danger">{props.errors.email}</small> : ""}
-  </div>
-  <div className="form-group">
-    <label htmlFor="password1">Password</label>
-    <input type="password" className="form-control" name="password" id="password1"  onChange={props.onChange} value={props.password} />
-    {props.errors.password ? <small className="text-danger">{props.errors.password}</small> : ""}
-  </div>
-  <div className="form-group">
-    <label htmlFor="password2">Confirm password</label>
-    <input type="password" className="form-control" name="password2" id="password2"  onChange={props.onChange} value={props.password2} />
-  </div>
-  <button onClick={props.onSubmit} className="btn btn-primary">Sign Up</button>
-</form>
+  <form>
+    {!!props.errors.global && (
+      <div class="alert alert-danger">{props.errors.global}</div>
+    )}
+    <div className="form-group">
+      <label htmlFor="email">Email address</label>
+      <input
+        type="email"
+        className="form-control"
+        name="email"
+        id="email"
+        onChange={props.onChange}
+        value={props.email}
+      />
+      {!!props.errors.email && (
+        <small className="text-danger">{props.errors.email}</small>
+      )}
+    </div>
+    <div className="form-group">
+      <label htmlFor="password1">Password</label>
+      <input
+        type="password"
+        className="form-control"
+        name="password"
+        id="password1"
+        onChange={props.onChange}
+        value={props.password}
+      />
+      {!!props.errors.password && (
+        <small className="text-danger">{props.errors.password}</small>
+      )}
+    </div>
+    <div className="form-group">
+      <label htmlFor="password2">Confirm password</label>
+      <input
+        type="password"
+        className="form-control"
+        name="password2"
+        id="password2"
+        onChange={props.onChange}
+        value={props.password2}
+      />
+    </div>
+    <button onClick={props.onSubmit} className="mt-4 btn btn-danger">
+      Sign Up
+    </button>
+  </form>
 );
 
 export default SignUp;
--- client/src/containers/Register.js
+++ client/src/containers/Register.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import axios from 'axios';
-import SignUpForm from '../components/Forms/SignUp';
+import axios from "axios";
+import SignUpForm from "../components/Forms/SignUp";
+import { Link } from "react-router-dom";
 
 export default class Register extends Component {
   state = {
@@ -8,52 +9,80 @@ export default class Register extends Component {
     password: "",
     password2: "",
     errors: {}
-  }
+  };
 
-  handleChange = (e) => {
-    this.setState({[e.target.name]: e.target.value})
-  }
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
 
-    const {email, password, password2} = this.state;
-    const errors = this.validate({email, password, password2});
-    this.setState({errors});
+    const { email, password, password2 } = this.state;
+    const errors = this.validate({ email, password, password2 });
+    this.setState({ errors });
 
-    if( Object.keys(errors).length === 0 ) {
-      axios.post('http://localhost:5000/v1/signup', {email, password}).then( user => {
-        if(user !== null) {
-          this.props.history.push("/login")
-        }
-      })
+    if (Object.keys(errors).length === 0) {
+      axios
+        .post("http://localhost:5000/v1/signup", { email, password })
+        .then(user => {
+          if (user.data.success) {
+            this.props.history.push("/login");
+          } else {
+            this.setState({
+              errors: { ...this.state.errors, global: user.data.message }
+            });
+          }
+        });
     }
-
-    console.log('Submitted...')
-  }
+  };
 
   validate = data => {
-    
     const errors = {};
-    
-    !data.email ? errors.email = "Enter your email address." : "";
-    !data.password ? errors.password = "Enter your password." : "";
-    data.password !== data.password2 ? errors.password = "Your passwords doesn't match." : "";
-    
-    console.log('data', errors);
+
+    !data.email ? (errors.email = "Enter your email address.") : "";
+    !data.password ? (errors.password = "Enter your password.") : "";
+    data.password !== data.password2
+      ? (errors.password = "Your passwords doesn't match.")
+      : "";
+
     return errors;
-  }
+  };
+
+  componentWillMount = () => {
+    if (localStorage.getItem("JWT")) {
+      this.props.history.push("/");
+    }
+  };
 
   render() {
-    console.log('errors', this.state.errors)
     return (
       <div className="mt-5">
-        <h1 className="text-center">Registration page</h1>
+        <div className="text-center mb-5">
+          <h1 className="mb-4 text-danger">Sign up... it's free!</h1>
+          <p className="text-muted">
+            Easily signup to our community and enjoy of multiple benefits we
+            offer for members. .
+          </p>
+          <p>
+            Already a member?{" "}
+            <Link to="/login" className="text-danger">
+              Sign in your account
+            </Link>
+          </p>
+        </div>
         <div className="container">
           <div className="row">
             <div className="col-md-7 m-auto">
-              <div className="bg-info p-5 text-white">
-              <SignUpForm onChange={this.handleChange} onSubmit={this.handleSubmit} errors={this.state.errors} email={this.state.email} password={this.state.password} password2={this.state.password2} />
+              <div className="bg-dark rounded p-5 text-white">
+                <SignUpForm
+                  onChange={this.handleChange}
+                  onSubmit={this.handleSubmit}
+                  errors={this.state.errors}
+                  email={this.state.email}
+                  password={this.state.password}
+                  password2={this.state.password2}
+                />
               </div>
             </div>
           </div>
