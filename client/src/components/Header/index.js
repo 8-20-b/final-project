5e7ab687468d966a68ea66bc49d8d7fd669d2de2
Dioni Mercado <me@dionimercado.com>
2018-09-30 18:13:14 -0400

    Mobile fixes and improvements

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -7,12 +7,26 @@ import SearchForm from "../Forms/Search";
 const Header = ({ brand, isAuth, logout }) => (
   <nav className="navbar navbar-expand-md navbar-dark bg-danger m-0 p-0">
     <NavLink
-      className="col-md-2 col-sm-3 mr-5 navbar-brand py-3 text-center"
+      className="col-md-2 col-sm-3 mr-0 mr-md-5 navbar-brand py-3 text-center pr-0"
       style={{ backgroundColor: "rgba(0,0,0,.1)" }}
       to="/"
     >
-      <img src="/images/logo.png" alt={brand} style={{ height: "32px" }} />
+      <img
+        className="d-none d-md-block"
+        src="/images/logo.png"
+        alt={brand}
+        style={{ height: "32px" }}
+      />
+      <img
+        className="d-block d-md-none"
+        src="/images/logo-mobile.png"
+        alt={brand}
+        style={{ height: "32px" }}
+      />
     </NavLink>
+    <div className="d-block d-md-none search-form">
+      <SearchForm />
+    </div>
     <button
       className="navbar-toggler"
       type="button"
@@ -21,8 +35,65 @@ const Header = ({ brand, isAuth, logout }) => (
     >
       <span className="navbar-toggler-icon" />
     </button>
-    <div className="collapse navbar-collapse" id="mobileNav">
-      <SearchForm />
+    <div className="d-block d-md-none w-100">
+      <div className="collapse navbar-collapse" id="mobileNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/movies/recently-added">
+              Recently Added
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/movies/new-releases/">
+              New Releases
+            </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/movies/upcoming">
+              Upcoming
+            </NavLink>
+          </li>
+          {isAuth && (
+            <React.Fragment>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/movies/favorites">
+                  Favorites
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/movies/watch-later">
+                  Watch Later
+                </NavLink>
+              </li>
+            </React.Fragment>
+          )}
+        </ul>
+        <div className="d-flex justify-content-between p-3 bg-dark">
+          {isAuth ? (
+            <button
+              onClick={() => logout()}
+              className="btn btn-block btn-outline-light"
+            >
+              Logout
+            </button>
+          ) : (
+            <React.Fragment>
+              <NavLink
+                to="/login"
+                className="btn btn-outline-light btn-block mr-2"
+              >
+                Log In
+              </NavLink>
+              <NavLink to="/register" className="btn btn-light btn-block ml-2">
+                Sign Up
+              </NavLink>
+            </React.Fragment>
+          )}
+        </div>
+      </div>
+    </div>
+    <div className="collapse navbar-collapse" id="mainNav">
       <ul className="navbar-nav ml-auto mr-3">
         {isAuth ? (
           <li className="nav-item">
